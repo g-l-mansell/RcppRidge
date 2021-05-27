c5a847f12860355af0deeda6d5525ae273ce8bfd
g-l-mansell <g.l.mansell@hotmail.co.uk>
2021-05-27 11:34:56 +0100

    Add utility functions

---
+++ analysis/0_utilityfunctions.R
@@ -0,0 +1,20 @@
+# Place to store useful functions for the project, that are too specific to be added to the package
+
+
+#our feature transform for the dataset of demand + extra
+feature_transform <- function(X){
+  
+  
+}
+
+#our feature transform for the dataset of demand + extra + surv
+feature_transform_surv <- function(X){
+  
+  
+}
+
+plot_monthly_demand <- function(){
+  
+}
+
+#etc.
