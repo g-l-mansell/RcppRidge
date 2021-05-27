017feb352f862137c2a55c1fefcb8f687303473a
g-l-mansell <g.l.mansell@hotmail.co.uk>
2021-05-27 17:08:27 +0100

    Update totals model to exclude tod

--- analysis/3_modeltotals.R
+++ analysis/3_modeltotals.R
@@ -1,29 +1,50 @@
 # Fit ridge regression model to the total demand data
 library(tidyverse)
 library(RcppRidge)
-load("data/Irish_Totals.RData")
+load("data/Irish_Processed.RData")
 source("analysis/0_utilityfunctions.R")
+
+
+### Create dataset of total demand per time
+df <- extra %>%
+  mutate(demand = rowSums(cust)) %>%
+  as.data.frame()
+
+#split input and response
+X <- df[, c(2, 3, 5)]
+y <- df$demand
+
+#subtract mean so we dont need an intercept
+mean_y <- mean(y)
+y <- y - mean(y)
+
+#feature transform
+X <- feature_transform(X)
+
+#remove the last day as a test
+y_test <- y[df$testSet]
+X_test <- X[df$testSet,]
+
+y <- y[!df$testSet]
+X <- X[!df$testSet,]
 
 #list of lambdas to test
 lambdas <- exp(seq(-2,4,length=20))
 
 #create group index, as we want to fit a model to each time of day separately
-idx <- as.numeric(as.factor(X[,"tod"]))
+idx <- df[!df$testSet,"tod"]
 
 #use par_reg to fit 48 models in parallel
 res <- par_reg(X, as.matrix(y), lambdas, idx)
-
-#extract results, betas is a ncol(X)x48 matrix with fitted betas
-#and lambdas is length-48 vector of fitted regularisation parameter for each group
-betas <- res$betas
-opt_lams <- res$lambdas
-vars <- res$variances
+betas <- res$betas # px48 matrix with fitted betas for each group
+opt_lams <- res$lambdas # length-48 vector of fitted lambda for each group
+vars <- res$variances # pxpx48 array of covariance matrix for beta|y
 
 #now predict total demand for the test set
-idx_y  <- as.numeric(factor(X_test[, "tod"], levels=levels(factor(X[,"tod"]))))
+idx_y <- df[df$testSet,"tod"]
 y_pred <- predict_groups(X_test, betas, idx_y)
 
-#View the predictions (looks fine)
+#view the predictions
 plot_monthly_predictions(y_test, cbind(NA, y_pred, NA))
 
 
@@ -37,8 +58,7 @@ y_pred_mat <- matrix(NA, nrow=length(y_test), ncol=n_samp)
 
 for(i in 1:48){
   #get matrix of X_test just for this tod
-  tod <- unique(X[,"tod"])[i]
-  sub <- X_test[,"tod"] == tod
+  sub <- df[df$testSet, "tod"] == (i-1)
   X_sub <- X_test[sub,]
   
   #sample betas
@@ -69,5 +89,27 @@ png("analysis/figures/MontlyPredictionsInterval.png", width=800, height=500)
 plot_monthly_predictions(y_test, y_int)
 dev.off()
 
-MSE <- mean(abs(y_test - y_pred)) / nrow(X_test)
+(MSE <- mean(abs(y_test - y_pred)) / nrow(X_test))
 #[1] 0.1422841
+
+
+
+#test plotting betas for each of the 48 models
+colnames(betas) <- 1:48 #seq(0, 23.5, 0.5)
+pd <- as.data.frame(betas) %>%
+  mutate(var=factor(colnames(X), levels=colnames(X))) %>%
+  pivot_longer(cols=-"var", names_to="tod", values_to="coeff") %>%
+  mutate(tod=as.numeric(tod))
+
+ggplot(pd, aes(x=var, y=coeff)) +
+  geom_boxplot(outlier.shape=NA) +
+  geom_jitter(aes(colour=tod), size=0.8, alpha=0.8) +
+  labs(title="Betas for 48 total demand models",
+       x=NULL, y="beta", colour="Model") +
+  theme_minimal() +
+  scale_colour_viridis_c()
+
+
+#test plotting betas_samp for one model
+par(mfrow=c(1,1))
+boxplot(betas_samp, names=colnames(X), ylab="Regression Coefficient", cex.axis=0.8)
